74a6f1c1f16ff238efc5c53ab76033046c53da34
Denver <aeharvlee@gmail.com>
2021-05-20 01:28:20 +0900

    Refactor testcases / Change gradle dependency
    Refactor testcases based on SDK usability improvement
    
    Change android tools gradle dependency because Intelli J cannot open this project because of this dependency.

--- core_android/src/androidTest/java/com/klaytn/caver/core_android/base/Accounts.java
+++ core_android/src/androidTest/java/com/klaytn/caver/core_android/base/Accounts.java
@@ -16,12 +16,31 @@
 
 package com.klaytn.caver.core_android.base;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.klaytn.caver.Caver;
+import com.klaytn.caver.methods.response.Bytes32;
+import com.klaytn.caver.methods.response.TransactionReceipt;
+import com.klaytn.caver.transaction.TxPropertyBuilder;
+import com.klaytn.caver.transaction.response.PollingTransactionReceiptProcessor;
+import com.klaytn.caver.transaction.response.TransactionReceiptProcessor;
+import com.klaytn.caver.transaction.type.ValueTransfer;
+import com.klaytn.caver.utils.Utils;
+import com.klaytn.caver.wallet.keyring.AbstractKeyring;
 import com.klaytn.caver.wallet.keyring.SingleKeyring;
+
+import org.web3j.protocol.exceptions.TransactionException;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
 import static com.klaytn.caver.core_android.base.LocalValues.ANDROID_DEFAULT_URL;
+import static com.klaytn.caver.core_android.base.LocalValues.KLAY_PROVIDER;
+import static com.klaytn.caver.core_android.base.LocalValues.LOCAL_CHAIN_ID;
 
 public class Accounts {
-    static Caver caver = new Caver(ANDROID_DEFAULT_URL);
+    private static Caver caver = new Caver(ANDROID_DEFAULT_URL);
     public static final SingleKeyring LUMAN = caver.wallet.keyring.create(
             "0x2c8ad0ea2e0781db8b8c9242e07de3a5beabb71a",
             "0x2359d1ae7317c01532a58b01452476b796a3ac713336e97d8d3c9651cc0aecc3"
@@ -42,4 +61,42 @@ public class Accounts {
             "0x9d0dcbe163be73163348e7f96accb2b9e1e9dcf6",
             "0x1e558ea00698990d875cb69d3c8f9a234fe8eab5c6bd898488d851669289e178"
     );
+
+    public static String objectToString(Object value) throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        return ow.writeValueAsString(value);
+    }
+
+    static {
+        caver.wallet.add(KLAY_PROVIDER);
+        try {
+            fillUpKlay(LUMAN);
+            fillUpKlay(WAYNE);
+            fillUpKlay(BRANDON);
+            fillUpKlay(FEE_PAYER);
+        } catch (IOException | TransactionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void fillUpKlay(AbstractKeyring recipient) throws IOException, TransactionException {
+        ValueTransfer valueTransfer = caver.transaction.valueTransfer.create(
+                TxPropertyBuilder.valueTransfer()
+                        .setFrom(KLAY_PROVIDER.getAddress())
+                        .setTo(recipient.getAddress())
+                        .setValue(caver.utils.convertToPeb("100", Utils.KlayUnit.KLAY))
+                        .setGas(BigInteger.valueOf(4300000))
+                        .setChainId(BigInteger.valueOf(LOCAL_CHAIN_ID))
+        );
+
+        caver.wallet.sign(KLAY_PROVIDER.getAddress(), valueTransfer);
+        Bytes32 sendResult = caver.rpc.klay.sendRawTransaction(valueTransfer.getRawTransaction()).send();
+        String txHash = sendResult.getResult();
+
+        TransactionReceiptProcessor receiptProcessor = new PollingTransactionReceiptProcessor(caver, 1000, 15);
+
+        TransactionReceipt.TransactionReceiptData receiptData = receiptProcessor.waitForTransactionReceipt(txHash);
+
+        System.out.println(objectToString(receiptData));
+    }
 }
--- core_android/src/androidTest/java/com/klaytn/caver/core_android/base/LocalValues.java
+++ core_android/src/androidTest/java/com/klaytn/caver/core_android/base/LocalValues.java
@@ -5,7 +5,7 @@ import com.klaytn.caver.wallet.keyring.SingleKeyring;
 
 public class LocalValues {
     public static final String ANDROID_DEFAULT_URL = "http://10.0.2.2:8551";
-    static Caver caver = new Caver(ANDROID_DEFAULT_URL);
+    private static Caver caver = new Caver(ANDROID_DEFAULT_URL);
     public static final SingleKeyring KLAY_PROVIDER = caver.wallet.keyring.createFromPrivateKey(
             "871ccee7755bb4247e783110cafa6437f9f593a1eaeebe0efcc1b0852282c3e5"
     );
