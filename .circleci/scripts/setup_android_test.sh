5f449ec861f41ebcc80a3a5eb58c1a098a64e635
Denver <aeharvlee@gmail.com>
2021-05-25 00:21:30 +0900

    Fix shell script syntax

--- .circleci/scripts/setup_android_test.sh
+++ .circleci/scripts/setup_android_test.sh
@@ -6,6 +6,6 @@ CORE_ANDROID_DIR="$PROJECT_DIR/core_android/src/androidTest/java/com/klaytn/cave
 
 cd $CORE_ANDROID_DIR
 
-cp -r "$CORE_DIR/*" .
-find  . -type -f -name '*.java' | xargs sed -i 's/package com.klaytn.caver/package com.klaytn.caver.core_android./g'
-find  . -type -f -name '*.java' | xargs sed -i 's/Caver.DEFAULT_URL/Caver.ANDROID_DEFAULT_URL/g' 
+cp -r $CORE_DIR/* .
+find  . -type f -name '*.java' | xargs sed -i 's/package com.klaytn.caver/package com.klaytn.caver.core_android./g'
+find  . -type f -name '*.java' | xargs sed -i 's/Caver.DEFAULT_URL/Caver.ANDROID_DEFAULT_URL/g'
