aac4d4c72261eb902eea37fbf75f3bc2b6619341
Denver <aeharvlee@gmail.com>
2021-05-25 14:20:39 +0900

    Update build gradle and setup script

--- .circleci/scripts/setup_android_test.sh
+++ .circleci/scripts/setup_android_test.sh
@@ -8,8 +8,8 @@ cd $CORE_ANDROID_DIR
 mkdir -p src/androidTest/java/com/klaytn/caver/core_android
 
 cp -r $CORE_DIR/* .
-find  . -type f -name '*.java' | xargs sed -i 's/package com.klaytn.caver/package com.klaytn.caver.core_android/g'
-find  . -type f -name '*.java' | xargs sed -i 's/import static com.klaytn.caver/import static com.klaytn.caver.core_android/g'
-find  . -type f -name '*.java' | xargs sed -i 's/import com.klaytn.caver.base/import com.klaytn.caver.core_android.base/g'
-find  . -type f -name '*.java' | xargs sed -i 's/Caver.DEFAULT_URL/Caver.ANDROID_DEFAULT_URL/g'
-find  . -type f -name '*.java' | xargs sed -i 's/localhost/10.0.2.2/g'
+find  . -type f -name '*.java' | grep -v "legacy" | grep -v "ipfs" | xargs sed -i 's/package com.klaytn.caver/package com.klaytn.caver.core_android/g'
+find  . -type f -name '*.java' | grep -v "legacy" | grep -v "ipfs" | xargs sed -i 's/import static com.klaytn.caver/import static com.klaytn.caver.core_android/g'
+find  . -type f -name '*.java' | grep -v "legacy" | grep -v "ipfs" | xargs sed -i 's/import com.klaytn.caver.base/import com.klaytn.caver.core_android.base/g'
+find  . -type f -name '*.java' | grep -v "legacy" | grep -v "ipfs" | xargs sed -i 's/Caver.DEFAULT_URL/Caver.ANDROID_DEFAULT_URL/g'
+find  . -type f -name '*.java' | grep -v "legacy" | grep -v "ipfs" | xargs sed -i 's/localhost/10.0.2.2/g'
