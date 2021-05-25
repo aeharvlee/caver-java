ce8636701c8fc7305502ea9c9cf812f0a8800467
Denver <aeharvlee@gmail.com>
2021-05-25 10:31:43 +0900

    Update setup script

--- .circleci/scripts/setup_android_test.sh
+++ .circleci/scripts/setup_android_test.sh
@@ -10,4 +10,5 @@ mkdir -p src/androidTest/java/com/klaytn/caver/core_android
 cp -r $CORE_DIR/* .
 find  . -type f -name '*.java' | xargs sed -i 's/package com.klaytn.caver/package com.klaytn.caver.core_android/g'
 find  . -type f -name '*.java' | xargs sed -i 's/import static com.klaytn.caver/import static com.klaytn.caver.core_android/g'
+find  . -type f -name '*.java' | xargs sed -i 's/import com.klaytn.caver.base/import com.klaytn.caver.core_android.base/g'
 find  . -type f -name '*.java' | xargs sed -i 's/Caver.DEFAULT_URL/Caver.ANDROID_DEFAULT_URL/g'
