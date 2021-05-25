dbaccf794c480118321de2c4a633d1c27fc395d7
Denver <aeharvlee@gmail.com>
2021-05-25 09:43:27 +0900

    Add static replacement

--- .circleci/scripts/setup_android_test.sh
+++ .circleci/scripts/setup_android_test.sh
@@ -5,7 +5,9 @@ CORE_DIR="$PROJECT_DIR/core/src/test/java/com/klaytn/caver"
 CORE_ANDROID_DIR="$PROJECT_DIR/core_android/src/androidTest/java/com/klaytn/caver/core_android"
 
 cd $CORE_ANDROID_DIR
+mkdir -p src/androidTest/java/com/klaytn/caver/core_android
 
 cp -r $CORE_DIR/* .
 find  . -type f -name '*.java' | xargs sed -i 's/package com.klaytn.caver/package com.klaytn.caver.core_android/g'
+find  . -type f -name '*.java' | xargs sed -i 's/import static com.klaytn.caver/import static com.klaytn.caver.core_android/g'
 find  . -type f -name '*.java' | xargs sed -i 's/Caver.DEFAULT_URL/Caver.ANDROID_DEFAULT_URL/g'
