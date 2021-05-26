05ed7db5131a4ff745bf6c61572213dafd734b63
Denver <aeharvlee@gmail.com>
2021-05-26 16:19:25 +0900

    Update script and caver
    Because Android Default Url is only used when do testing,
    we don't have to put it in Caver class.

--- .circleci/scripts/setup_android_test.sh
+++ .circleci/scripts/setup_android_test.sh
@@ -18,5 +18,5 @@ cp -r $CORE_TEST_CAVER_DIR/common $ANDROID_TEST_CAVER_DIR/
 find  $ANDROID_TEST_CAVER_DIR -type f -name '*.java' | xargs sed -i 's/package com.klaytn.caver/package com.klaytn.caver.android_instrumented_test/g'
 find  $ANDROID_TEST_CAVER_DIR -type f -name '*.java' | xargs sed -i 's/import static com.klaytn.caver/import static com.klaytn.caver.android_instrumented_test/g'
 find  $ANDROID_TEST_CAVER_DIR -type f -name '*.java' | xargs sed -i 's/import com.klaytn.caver.base/import com.klaytn.caver.android_instrumented_test.base/g'
-find  $ANDROID_TEST_CAVER_DIR -type f -name '*.java' | xargs sed -i 's/Caver.DEFAULT_URL/Caver.ANDROID_DEFAULT_URL/g'
+find  $ANDROID_TEST_CAVER_DIR -type f -name '*.java' | xargs sed -i 's/Caver.DEFAULT_URL/"http:\/\/10.0.2.2:8551"/g'
 find  $ANDROID_TEST_CAVER_DIR -type f -name '*.java' | xargs sed -i 's/localhost/10.0.2.2/g'
